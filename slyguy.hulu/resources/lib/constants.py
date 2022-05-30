b8a907cb2d3c93babe191409e1d97911b95991dc
liam8888999 <79149245+liam8888999@users.noreply.github.com>
2022-05-31 01:05:26 +1000

    Delete slyguy.hulu directory

--- slyguy.hulu/resources/lib/constants.py
+++
@@ -1,9 +0,0 @@
-HEADERS = {
-    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/94.0.4606.81 Safari/537.36',
-}
-
-DEVICE_ACTIVATE_URL = 'https://hulu.com/activate'
-API_URL = 'https://discover.hulu.com{}'
-
-DEEJAY_DEVICE_ID = 188
-DEEJAY_KEY_VERSION = 17
