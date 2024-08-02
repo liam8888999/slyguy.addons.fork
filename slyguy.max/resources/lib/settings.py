32b4535f2eb959b433d53874257ce0a1e5128ac8
liam8888999 <robsonliam2@gmail.com>
2024-08-03 01:27:51 +1000

    remove unused

--- slyguy.max/resources/lib/settings.py
+++
@@ -1,9 +0,0 @@
-from slyguy.settings import CommonSettings
-from slyguy.settings.types import Bool
-
-
-class Settings(CommonSettings):
-    ENABLE_CHAPTERS = Bool('enable_chapters', default=False)
-
-
-settings = Settings()
